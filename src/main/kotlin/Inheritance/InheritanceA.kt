bc293e0b7de0e01eff4d5e4c43ba68b5bd892c1a
RPL-24 <malydaauliya@gmail.com>
2022-03-31 14:32:30 +0700

    membuat kelas parent dan child

---
+++ src/main/kotlin/Inheritance/InheritanceA.kt
@@ -0,0 +1,20 @@
+package Inheritance
+
+open class Computer(val name: String,
+                    val brand: String) {
+}
+
+class Laptop(name: String,
+             brand: String,
+             val batteryLife: Double) : Computer(name, brand) {
+    fun info(){
+        println("Name : $name")
+        println("Brand : $brand")
+        println("Battery Life : $batteryLife")
+    }
+             }
+
+fun main(){
+    val myLaptop = Laptop("Acer Aspire 4738", "Acer", 2.5)
+    println(myLaptop.info())
+}
